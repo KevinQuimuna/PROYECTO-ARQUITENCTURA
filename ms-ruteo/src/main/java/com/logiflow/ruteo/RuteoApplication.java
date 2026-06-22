b76a978762003a7c080c7ce8336529db34a87b23
Mishell Estefania Cuasquer Chisaguano <121051282+KevinQuimuna@users.noreply.github.com>
2026-06-22 13:46:46 -0500

    Fix: ajustes en RuteoApplication

--- ms-ruteo/src/main/java/com/logiflow/ruteo/RuteoApplication.java
+++ ms-ruteo/src/main/java/com/logiflow/ruteo/RuteoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+//DEPRUEBAS
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
